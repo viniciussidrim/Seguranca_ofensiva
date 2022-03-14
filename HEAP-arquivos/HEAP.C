18d4935ccead4c5a963ea8aeb7620a97aeb4cdc1
Vinícius Sidrim <viniciussidrim99@gmail.com>
2022-03-14 20:48:05 -0300

    Arquivos de vericação de memória em C

---
+++ HEAP-arquivos/HEAP.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *arg[]){
+    char c1;
+    char c2;
+    char c3;
+    char c4;
+    char *ponteiro;
+
+    ponteiro=(char*) malloc(1024*1024);
+    scanf("%c", &c1);
+
+    return 0;
+}
